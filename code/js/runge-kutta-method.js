1a34c60fd623e84833a17bc4445b7a7f73a2333e
gaan3654 <g.anuzyte@yahoo.com>
2019-05-09 14:25:21 +0300

    Prograsuojam su RK metodu

--- code/js/runge-kutta-method.js
+++ code/js/runge-kutta-method.js
@@ -8,6 +8,10 @@ function k1(t, y, h, func){
 }
 
 function k2(t, y, h, k1, func){
+    var y_new = [];
+    for(var i = 0; i < y.length; i++){
+        
+    }
     return h * f(t+h/2,y+k1/2, func);
 }
 
@@ -21,17 +25,39 @@ function k4(t, y, h, k3, func){
 
 function solution(y0, a, b, N, func){
     var t=[];
-    var y=[];
-    y[0] = y0;
+    //daugiamatis masyvas su iniializuotom pradinėm medžiagų konsentracijom
+    var y =[];
+    for(var i = 0; i<y0.length; i++){
+        y.push([y0[i]]);
+    }
+    //console.log(func);
+    
+    //console.log(y[0][y[0].length-1]);
+    //y[0] = y0;
     t[0] = a;
     var h= (b-a)/N;
     for (var i=0; i < N; i++) {
+        for(var j = 0; j < y.length; j++){
+            func = func.replace(/A|B|C|D/, `y[${j}][${y[j].length-1}]`);
+            //console.log(func);
+        }
         t[i+1] = t[i]+h;
-        var kk1 = k1(t[i], y[i], h, func);
-        var kk2 = k2(t[i], y[i], h, kk1, func);
-        var kk3 = k3(t[i], y[i], h, kk2, func);
-        var kk4 = k4(t[i], y[i], h, kk3, func);
-        y[i+1] = y[i]+1/6*(kk1+2*kk2+2*kk3+kk4);
+        console.log(t[i]," t[i]---", y[0], " y[0]---", h," h---"    , func, " func");
+        var kk1 = k1(t[i], y, h, func);
+        console.log(t[i]," t[i]---", y[0], " y[0]---", h," h---"    , func, " func---", kk1, " kk1");
+        var kk2 = k2(t[i], y, h, kk1, func);
+        console.log(t[i]," t[i]---", y[0], " y[0]---", h," h---"    , func, " func---", kk1, " kk1---", kk2, " kk2");
+        var kk3 = k3(t[i], y, h, kk2, func);
+        var kk4 = k4(t[i], y, h, kk3, func);
+        //  y[i+1] = y[i]+1/6*(kk1+2*kk2+2*kk3+kk4);
+        // console.log(kk1);
+        // console.log(kk2);
+        // console.log(kk3);
+        // console.log(kk4);
+        for(var j = 0; j < y.length; j++){
+            y[j][i+1] = y[j][i]+1/6*(kk1+2*kk2+2*kk3+kk4);
+            //console.log(y[j][i+1]);
+        }
     } 
     return [y,t];
 }
