81160b04d54891ba67a41feced5410e7125daae8
dbarilovic <48729814+dbarilovic@users.noreply.github.com>
2019-06-12 08:54:31 +0300

    grafikas neliečia canvaso viršaus
    grafikas neliečia canvaso viršaus

--- code/js/runge-kutta-method.js
+++ code/js/runge-kutta-method.js
@@ -166,7 +166,10 @@ function normalization(yy,tt, a, b, height, width){
     for(var i=0; i<yy.length; i++){
         var yp_temp = [];
         for(var j=0; j<yy[i].length; j++){
-            yp_temp[j] = ((height-20)-(k*yy[i][j]+l)*(height-20));
+            yp_temp[j] = ((height-40)-(k*yy[i][j]+l)*(height-40))+20;
+            //šitoj vietoj pakeičiau normalizavimą
+            // buvo taip: yp_temp[j] = ((height-20)-(k*yy[i][j]+l)*(height-20));
+            //čia tuos dvidešimt pridedu kad atlipintų grafiką nuo viršaus
             yy[i][j]=yp_temp[j];
             if(i==0){
                 tp[j] = ((kt*tt[j]+lt)*(width-20))+20;
