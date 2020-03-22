8b6f026f7ac2898e7510c75604dec8082c7ec48b
gaan3654 <g.anuzyte@yahoo.com>
2020-03-22 16:48:17 +0200

    Cleaned up code
    Although the chart does not get displayed

---
+++ code/js/normalization.js
@@ -0,0 +1,45 @@
+let y_min = 0;
+let y_max;
+//Gautų rezultatų normalizavimas
+function normalization(y, t, a, b, height, width) {
+  let yp = [];
+  let tp = [];
+
+  let y_maxRow = y.map(function(row) {
+    return Math.max.apply(Math, row);
+  });
+  y_max = Math.max.apply(null, y_maxRow);
+
+  //yy normalizacija
+  let k_y = 1;
+  let l_y = 1;
+  if (y_min != y_max) {
+    k_y = 1 / (y_max - y_min);
+    l_y = -y_min / (y_max - y_min);
+  }
+  //tt normalizacija
+  let k_t = 1 / (b - a);
+  let l_t = -a / (b - a);
+
+  for (let i = 0; i < y.length; i++) {
+    let yp_temp = [];
+    for (let j = 0; j < y[i].length; j++) {
+      yp_temp[j] = height - 20 - (k_y * y[i][j] + l_y) * (height - 20);
+      y[i][j] = yp_temp[j];
+      if (i == 0) {
+        tp[j] = (k_t * t[j] + l_t) * (width - 20) + 20;
+      }
+    }
+    yp.push(yp_temp);
+  }
+  return [yp, tp];
+}
+
+function getRandomColor() {
+  var symbols = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += symbols[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
--- code/js/runge-kutta-method.js
+++ code/js/runge-kutta-method.js
@@ -1,193 +1,70 @@
-
-//Runge–Kutta metodas
-function f(t,y, func){
-    let result;
-    // console.log(func);
-    result = eval(func);
-    return result;
+function evaluateFunction(t, y, func) {
+  return eval(func);
 }
 
-function k1(t, y, h, func){
-    return h * f(t, y, func);
+function calculateK1(t, y, h, func) {
+  return h * evaluateFunction(t, y, func);
 }
 
-function k2(t, y, h, k1, func){
-    //Nuklonuojamas y masyvas į y_copy
-    var y_copy = [];
-    for(var i = 0; i < y.length; i++){
-        var y_temp = [];
-        for(var j = 0; j < y[i].length; j++){
-            y_temp[j] = y[i][j];
-        }
-        y_copy.push(y_temp);
-    }
-    //Prie konsentracijos reikšių pridedama k1/2
-    for(var i = 0; i < y_copy.length; i++){
-        y_copy[i][y_copy[i].length-1] = eval(y_copy[i][y_copy[i].length-1] + k1[i] / 2);
-    }
-    return h * f(t+h/2, y_copy, func);
-}   
-
-function k3(t, y, h, k2, func){
-    //Nuklonuojamas y masyvas į y_copy
-    var y_copy = [];
-    for(var i = 0; i < y.length; i++){
-        var y_temp = [];
-        for(var j = 0; j < y[i].length; j++){
-            y_temp[j] = y[i][j];
-        }
-        y_copy.push(y_temp);
-    }
-    //Prie konsentracijos reikšių pridedama k2/2
-    for(var i = 0; i < y_copy.length; i++){
-        y_copy[i][y_copy[i].length-1] = eval(y_copy[i][y_copy[i].length-1] + k2[i] / 2);
-    }
-    return h * f(t+h/2, y_copy, func);
+function calculateK(t, y, h, k_old, func) {
+  let y_copy = copyArray(y);
+  for (let i = 0; i < y_copy.length; i++) {
+    y_copy[i][y_copy[i].length - 1] = eval(
+      y_copy[i][y_copy[i].length - 1] + k_old[i] / 2
+    );
+  }
+  return h * evaluateFunction(t + h / 2, y_copy, func);
 }
 
-function k4(t, y, h, k3, func){
-    //Nuklonuojamas y masyvas į y_copy
-    var y_copy = [];
-    for(var i = 0; i < y.length; i++){
-        var y_temp = [];
-        for(var j = 0; j < y[i].length; j++){
-            y_temp[j] = y[i][j];
-        }
-        y_copy.push(y_temp);
-    }
-    //Prie konsentracijos reikšių pridedama k3
-    for(var i = 0; i < y_copy.length; i++){
-        y_copy[i][y_copy[i].length-1] = eval(y_copy[i][y_copy[i].length-1] + k3[i]);
-    }
-    return h * f(t+h, y_copy, func);
+function copyArray(array) {
+  return array.map(function(arr) {
+    return arr.slice();
+  });
 }
 
-//Atnaujinamos funkcijų reikšmės kiekvienam k
-function renew_function(y, func_array){
-    for(let m = 0; m < y.length; m++){
-        func_array = func_array.replace(`y[${m}][${y[m].length-2}]`, `y[${m}][${y[m].length-1}]`);
-    }
-    return func_array;
+function generateKArray(t, y, h, substance_obj, previous_kk) {
+  let kk = [];
+  for (let j = 0; j < y.length; j++) {
+    substance_obj[j].function = renewFunction(y, j, substance_obj, false);
+    kk.push(calculateK(t, y, h, previous_kk, substance_obj[j].function));
+  }
+  return kk;
 }
 
-//Pradinė funkcija inicializuojama pradinėmis koncentracijomis; Raidės pakeičiamos atitinkamom y masyvo pozicijom
-function renew_init_f(original_f, substance_obj, m){
-    if(original_f.match(/A|B|C|D|E|F|G|H|I|J|K|N|L|O|P|Q|R|S|T|U|V|W|X|Y|Z/)){
-        let current_subs = (original_f.match(/A|B|C|D|E|F|G|H|I|J|K|N|L|O|P|Q|R|S|T|U|V|W|X|Y|Z/))[0];
-        let new_subs;
-        for(let i = 0; i < substance_obj.length; i++){
-            if(substance_obj[i][current_subs]){
-                new_subs = substance_obj[i][current_subs];
-            }
-        }
-        original_f = original_f.replace(new RegExp(current_subs, 'g'), new_subs);
-    }
-    return original_f;
+let y = [];
+let t = [];
+let h;
+let N;
+
+function initializeValues(big_n, a, b, substance_obj) {
+  t[0] = a;
+  let y_to_set = [];
+  for (let i = 0; i < substance_obj.length; i++) {
+    y_to_set.push([substance_obj[i].initial_conc]);
+  }
+  y = y_to_set;
+  h = (b - a) / big_n;
+  N = big_n;
 }
 
-//Pašalinami duplikatai iš paduodamo masyvo
-function remove_duplicates(arr) {
-    var obj = {};
-    var ret_arr = [];
-    for (var i = 0; i < arr.length; i++) {
-        obj[arr[i]] = true;
+function solution(substance_obj) {
+  for (let i = 0; i < N; i++) {
+    t[i + 1] = t[i] + h;
+    let kk1 = [];
+    //a+b+b->c kol kas neveikia
+    for (let j = 0; j < y.length; j++) {
+      let func = renewFunction(y, j, substance_obj, true);
+      kk1.push(calculateK1(t[i], y, h, func, substance_obj[j]));
     }
-    for (var key in obj) {
-        ret_arr.push(key);
-    }
-    return ret_arr;
-}
+    let kk2 = generateKArray(t[i], y, h, substance_obj, kk1);
+    let kk3 = generateKArray(t[i], y, h, substance_obj, kk2);
+    let kk4 = generateKArray(t[i], y, h, substance_obj, kk3);
 
-//Differencialinės lygties sprendimas Runge-Kutta metodu
-function solution(substance_obj, a, b, N, func_array, get_reaction){
-    var left_subs_count = get_reaction[0].split('');
-    left_subs_count = remove_duplicates(left_subs_count);
-    var func;
-    var t=[];
-    //daugiamatis masyvas inicializuojamas su pradinėm medžiagų konsentracijom
-    var y = [];
-    for(var i = 0; i<substance_obj.length; i++){
-        y.push([substance_obj[i].initial_conc]);
+    for (let j = 0; j < y.length; j++) {
+      y[j][i + 1] = eval(
+        y[j][i] + (1 / 6) * (kk1[j] + 2 * kk2[j] + 2 * kk3[j] + kk4[j])
+      );
     }
-    t[0] = a;
-    var h= (b-a)/N;
-    for (var i = 0; i < N; i++) {
-        t[i+1] = t[i]+h;
-        var kk1 = [];
-        var kk2 = [];
-        var kk3 = [];
-        var kk4 = [];
-        //Esant B konstantai reikia abiejų pusių diff - ant lapelio surašytos funkcijos eiga
-        //a+b+b->c kol kas galima palikti ramybėj
-        //Sugeneruojamos tinkamos funkcijos kiekvieno k apskaičiavimui.
-        for(var j = 0; j < y.length; j++){
-            //Pirmiausia raidės esančios funkcijoje pakeičiamos atitinkamomis y masyvo pozicjims
-            for(let m = 0; m < y.length; m++){
-                substance_obj[j].function = renew_init_f(substance_obj[j].function, substance_obj, m);
-                func = substance_obj[j].function;
-            }
-            kk1.push(k1(t[i], y, h, func, substance_obj[j]));
-        }
-        //Toliau pirminės medžiagų koncentracijos pakeičiamos naujomis; priskiriama sekanti reikšmė y masyve
-        for(var j = 0; j < y.length; j++){
-            substance_obj[j].function = renew_function(y, substance_obj[j].function);
-            kk2.push(k2(t[i], y, h, kk1, substance_obj[j].function));
-        }
-        for(var j = 0; j < y.length; j++){
-            substance_obj[j].function = renew_function(y, substance_obj[j].function);
-            kk3.push(k3(t[i], y, h, kk2, substance_obj[j].function));
-        }
-        for(var j = 0; j < y.length; j++){
-            substance_obj[j].function = renew_function(y, substance_obj[j].function);
-            kk4.push(k4(t[i], y, h, kk3, substance_obj[j].function));
-        }
-
-        for(var j = 0; j < y.length; j++){
-            y[j][i+1] = eval(y[j][i]+1/6*(kk1[j]+2*kk2[j]+2*kk3[j]+kk4[j]));
-        }
-    }
-    return [y,t];
-}
-
-var y_min=0;
-var y_max;
-//Gautų rezultatų normalizavimas
-function normalization(yy,tt, a, b, height, width){
-    var yp = [];
-    var tp = [];
-    y_max=yy[0][0];
-    for(var i=0; i<yy.length; i++){
-        for(var j=0; j<yy[i].length; j++){
-            //Jei y_min ir y_max sutampa tai if'ą
-            if(y_max<yy[i][j]){
-                y_max=yy[i][j];
-            }
-        }
-    }
-    //yy normalizacija
-    if(y_min != y_max){
-        var k = 1/(y_max-y_min);
-        var l = -y_min/(y_max-y_min);
-    } else {
-
-    }
-    //tt normalizacija
-    var kt = 1/(b-a);
-    var lt = (-a)/(b-a);
-    
-    for(var i=0; i<yy.length; i++){
-        var yp_temp = [];
-        for(var j=0; j<yy[i].length; j++){
-            yp_temp[j] = ((height-40)-(k*yy[i][j]+l)*(height-40))+20;
-            //šitoj vietoj pakeičiau normalizavimą
-            // buvo taip: yp_temp[j] = ((height-20)-(k*yy[i][j]+l)*(height-20));
-            //čia tuos dvidešimt pridedu kad atlipintų grafiką nuo viršaus
-            yy[i][j]=yp_temp[j];
-            if(i==0){
-                tp[j] = ((kt*tt[j]+lt)*(width-20))+20;
-            }
-        }
-        yp.push(yp_temp);
-    }
-    return [yp,tp];
+  }
+  return [y, t];
 }
