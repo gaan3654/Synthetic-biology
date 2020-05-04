75ecb22c8bc335ce9a8c113b0aac05b42e0508b9
dbarilovic <48729814+dbarilovic@users.noreply.github.com>
2020-05-04 23:37:24 +0300

    ode calculator examples
    adding some examples for ode caltulation

--- code/ode/ode-algorithm/examples.js
+++ code/ode/ode-algorithm/examples.js
@@ -1,32 +1,73 @@
 $("#example1").click(function() {
+  $("#water_production").css("display", "block");
   $("#reactions").load(" #reactions > *", function() {
     document.getElementById("int_ends").value = "1";
     document.getElementById("n_inp").value = "100";
-    document.getElementById("reaction_input_0").value = "A+B->C+B";
+    document.getElementById("second_reaction_rate_0").value = "0";
+    document.getElementById("reaction_input_0").value = "2*A+B->2*C";
     document.getElementById("A").value = "3";
     document.getElementById("B").value = "2";
-    document.getElementById("C").value = "1";
+    document.getElementById("C").value = "0";
   });
   reactions_id = ["reaction_0"];
   last_added_reaction_id = -1;
   exp3_clicked = "no";
 });
 
+// $("#example1").click(function() {
+//   $("#water_production").css("display", "block");
+//   $("#reactions").load(" #reactions > *", function() {
+//     document.getElementById("int_ends").value = "1";
+//     document.getElementById("n_inp").value = "100";
+//     document.getElementById("reaction_input_0").value = "A+B->C+B";
+//     document.getElementById("A").value = "3";
+//     document.getElementById("B").value = "2";
+//     document.getElementById("C").value = "1";
+//   });
+//   reactions_id = ["reaction_0"];
+//   last_added_reaction_id = -1;
+//   exp3_clicked = "no";
+// });
+
 $("#example2").click(function() {
+  $("#photosynthesis").css("display", "block");
   $("#reactions").load(" #reactions > *", function() {
-    document.getElementById("int_ends").value = "5";
+    document.getElementById("int_ends").value = "1";
     document.getElementById("n_inp").value = "100";
-    add_additional_reaction_input();
-    document.getElementById("reaction_input_0").value = "A->B";
-    document.getElementById("reaction_input_1").value = "C->0";
+    document.getElementById("reaction_rate_0").value = "0.5";
+    document.getElementById("second_reaction_rate_0").value = "0";
+    // add_additional_reaction_input();
+    document.getElementById("reaction_input_0").value = "6*A+6*B->C+6*D";
+    // document.getElementById("reaction_input_1").value = "C->0";
+    add_additional_substance_input();
     document.getElementById("A").value = "3";
     document.getElementById("B").value = "2";
-    document.getElementById("C").value = "1";
+    document.getElementById("C").value = "0";
+    document.getElementById("D").value = "0";
+    
   });
   reactions_id = ["reaction_0"];
   last_added_reaction_id = -1;
   exp3_clicked = "no";
 });
+
+// $("#example2").click(function() {
+//   $("#photosynthesis").css("display", "block");
+//   $("#reactions").load(" #reactions > *", function() {
+//     document.getElementById("int_ends").value = "5";
+//     document.getElementById("n_inp").value = "100";
+//     add_additional_reaction_input();
+//     document.getElementById("reaction_input_0").value = "A->B";
+//     document.getElementById("reaction_input_1").value = "C->0";
+//     document.getElementById("A").value = "3";
+//     document.getElementById("B").value = "2";
+//     document.getElementById("C").value = "1";
+    
+//   });
+//   reactions_id = ["reaction_0"];
+//   last_added_reaction_id = -1;
+//   exp3_clicked = "no";
+// });
 
 var exp3_clicked = "no";
 
