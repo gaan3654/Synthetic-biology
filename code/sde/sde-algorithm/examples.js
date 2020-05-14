40078bdf08dc0b2e7df083ea38d001315c9f3347
dbarilovic <48729814+dbarilovic@users.noreply.github.com>
2020-05-14 23:52:04 +0300

    Update examples.js
    updated covid-19 example

--- code/sde/sde-algorithm/examples.js
+++ code/sde/sde-algorithm/examples.js
@@ -18,19 +18,25 @@ $("#example1").click(function () {
 $("#example2").click(function () {
   $("#covid19").css("display", "block");
   $("#reactions").load(" #reactions > *", function () {
-    addSubstances(3);
-    addReaction(2);
+    addSubstances(4);
+    addReaction(3);
     document.getElementById("int_ends").value = "7";
     document.getElementById("n_inp").value = "700";
-    document.getElementById("A").value = "200";
-    document.getElementById("B").value = "15";
+    document.getElementById("A").value = "5000";
+    document.getElementById("B").value = "5";
     document.getElementById("C").value = "0";
+    document.getElementById("D").value = "0";
     document.getElementById("reaction_input_0").value = "A->B";
-    document.getElementById("reaction_rate_0").value = "0.25";
-    document.getElementById("second_reaction_rate_0").value = "0.6";
+    document.getElementById("reaction_rate_0").value = "0.02";
+    document.getElementById("second_reaction_rate_0").value = "0";
     document.getElementById("reaction_input_1").value = "B->C";
-    document.getElementById("reaction_rate_1").value = "0.04";
+    document.getElementById("reaction_rate_1").value = "0.015";
     document.getElementById("second_reaction_rate_1").value = "0";
+    document.getElementById("reaction_input_2").value = "B->D";
+    document.getElementById("reaction_rate_2").value = "0.004";
+    document.getElementById("second_reaction_rate_2").value = "0";
+    document.getElementById("int_ends").value = "21";
+    document.getElementById("n_inp").value = "2100";
     document.getElementById("iterations").value = "14";
     document.getElementById("show-only-mean").checked = false;
     document.getElementById("probability-interval-begin").value = "20";
